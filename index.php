6ca9bee88d83facd5830fff81ab7b0aadd8b9d62
lornamicheller <37231071+lornamicheller@users.noreply.github.com>
2019-05-03 14:18:24 -0400

    lastComiit_webDone

---
+++ index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Ladder LLC</title>
+    <meta name="description" content="Ladder LLC">
+
+    <meta name="csrf-param" content="authenticity_token" />
+    <meta name="csrf-token" content="WLY09jUbH4NofQVaw6eS3ZqsaydVzJdbiUjlD30qi4ddzMvz/cgwHXS6ubor+NT40ND4DDiS8pXc3/gxFcfFcA==" />
+    <meta name="title" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
+    <link rel="shortcut icon" type="image/png" href="favicon.png">
+    <meta name="msapplication-config" content="https://yalantis.com/assets/favicons/browserconfig-cc38834cf6dc3c517d841d63061e16beea5aa8267ecb8fd17b965554d9830e0a.xml" />
+    <link rel="stylesheet" media="screen" href="assets/v3/application-f0879836a12e7bc04945e5e09fdb379c2e958151429b5e2b0404dd353fe65327.css" />
+    <!--[if lt IE 8]><script>document.getElementsByTagName('html')[0].className += ' lt-ie9'</script><![endif]-->
+    <!--[if lte IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <!--[if lt IE 9]><script src="js/libs/css3-mediaqueries.js"></script><![endif]-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymo  us">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-31105176-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', "UA-31105176-1");
+    </script>
+</head>
+
+<body class='scene--bg-white-smoke' data-page="home" data-action="index">
+    <div class="page-loader">
+        <div class="page-loader__content">
+            <div class="page-loader__content-inner"><span class="page-loader__letter">L</span><span class="page-loader__letter">a</span><span class="page-loader__letter">d</span><span class="page-loader__letter">d</span><span class="page-loader__letter">e</span><span class="page-loader__letter">r</span><span class="page-loader__letter">L</span><span class="page-loader__letter">L</span><span class="page-loader__letter">C</span></div>
+        </div>
+    </div>
+    <header style="background-color: transparent;" class="page-header">
+        <div class="container">
+            <a class="logo" rel="nofollow" href="index.php">
+                <img class="logo__img" alt="Logo" src="img/ladder-logo.PNG" /> <span style="font-family: 'Poppins', sans-serif; font-size: 25px; position: absolute; width: 200px; top: 10px; left: 50px; color: white;">Ladder
+          LLC</span>
+            </a>
+            <button id="headerButtonBurger" class="main-nav-toggler" type="button">
+                <div class="icon-burger"><span class="icon-burger__bar"></span><span class="icon-burger__bar"></span><span class="icon-burger__bar"></span></div>
+            </button>
+            <nav class="main-nav">
+                <ul class="main-nav__list">
+                    <li class="main-nav__item"><a rel="nofollow" class="main-nav__link" href="#home">HOME</a>
+                    </li>
+                    <li class="main-nav__item"><a rel="nofollow" class="main-nav__link" href="#projects">PROJECTS</a></li>
+                    <li class="main-nav__item"><a rel="nofollow" class="btn btn--bordered" href="#contact">CONTACT
+              US</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main class="page-content">
+        <section id="home" class="section section--top section--top-home" style="background-image: url('img/iphone.jpg'); background-size: cover; background-attachment: fixed;">
+
+            <div class="container">
+                <div class="section__heading">
+                    <h1 class="section__title" style="font-size: 30px; color: white;">Mobile App Development</h1>
+                    <p class="section__subtitle" style="font-weight: 700; font-size: 80px; margin-top: 0; color: white;">ONE STEP FURTHER</p>
+                </div>
+            </div>
+            <div class="loading"><span class="loading-text">scroll down</span>
+                <div class="loading-dots"><span class="loading-dots__dot"></span><span class="loading-dots__dot"></span><span class="loading-dots__dot"></span></div>
+            </div>
+        </section>
+
+        <section id="projects" class="section" data-bg="black">
+            <div class="container-fluid ipad-bg" style="background-image: url('img/ipad-pen.jpg'); height: 100vh; background-attachment: fixed; background-size: cover;">
+                <p style="font-family: BrandonText, Helvetica, Arial, sans-serif; font-size: 100px; font-weight: 700; padding-top: 250px; color: white;">
+                    Our Projects</p>
+            </div>
+        </section>
+
+        <section class="section" data-bg="black" data-offset="-75" data-trigger-hook="0" data-duration=".2">
+            <div class="container">
+                <div class="section__heading">
+                    <h2 class="section__title" style="font-size: 60px; color: white;">FIaly - iOS</h2>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-4 col-md-4 col-sm-12">
+                                <img src="img/homescreen.png" width="300px" height="500px">
+                            </div>
+                            <div class="col-md-4 col-md-4 col-sm-12">
+                                <img src="img/paycheck.png" width="300px" height="500px">
+                            </div>
+                            <div class="col-md-4 col-md-4 col-sm-12">
+                                <img src="img/details.png" width="300px" height="500px">
+                            </div>
+                        </div>
+                    </div>
+                    <p class="section__intro" style="color: white; margin-top: 100px;">Hello, my name is Fialy, I’m a financial assistant.
+                        <br> Based on inflows and outflows, I will help with your daily finances.
+                        <br> You will have a daily balance available, allowing you to budget your daily outflows in
+                        <br> order to achieve your financial goals.
+                        <br> In the list below, you will find other things I can do for you.
+                        <br> Logging:
+                        <br> Monthly paycheck inflow and outflow.
+                        <br> Daily inflow and outflow (reminder available if enabled)
+                        <br> Forecast:
+                        <br> Daily balance
+                    </p>
+
+                    <div class="row justify-content-center my-4">
+                        <div class="col-md-6 col-sm-6 text-center">
+                            <button class="download" style="font-size: 13px;"><i style="font-size: 15px;" class="fab fa-apple"></i> Download</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="contact" class="section" data-bg="black" data-offset="-75" data-trigger-hook="0" data-duration=".2" style=" margin: 0; padding: 0;">
+            <div class="container-fluid" style="margin-top: 30px; border-top: 1px solid #f16522">
+                <h1 style="color: white; margin-top: 100px;">Contact Us</h1>
+                <form id="myform" method="post" name="contact_form" action="contact-form-handler.php">
+                    <fieldset>
+                        <div class="row justify-content-center my-4">
+                            <div class="col-md-8 col-sm-8">
+                                <input class="w-100 p-2 name-field" type="text" tabindex="1" id="name" name="name" value="" placeholder="NAME" style="backgrund: transparent;">
+                                <div class="text-right text-danger p-2 field_required_1"></div>
+                            </div>
+                        </div>
+
+                        <div class="row justify-content-center my-4">
+                            <div class="col-md-8 col-sm-8">
+                                <input class="w-100 p-2" type="email" tabindex="2" id="email" name="email" value="" placeholder="EMAIL">
+                                <div class="text-right text-danger p-2 field_required_1"></div>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center my-4">
+                            <div class="col-md-8 col-sm-8">
+                                <input class="w-100 p-2" type="tel" tabindex="3" id="phone" name="phone" value="" placeholder="PHONE">
+                                <div class="text-right text-danger p-2 field_required_1"></div>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center my-4">
+                            <div class="col-md-8 col-sm-8">
+                                <textarea class="w-100 p-2" id="message" name="message" tabindex="5" cols="40" rows="5" placeholder="MESSAGE"></textarea>
+                                <div class="text-right align-text-top text-danger p-2 field_required_2"></div>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center my-4">
+                            <div class="col-md-8 col-sm-8 text-center">
+                                <input type="submit" value="SUBMIT" id="submit" name="submit">
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </section>
+
+        <section style="padding: 0; margin: 0; height: auto; border-top: 1px solid #f16522; padding-bottom: 40px;" data-bg="black" data-offset="-75" data-trigger-hook="0" data-duration=".2">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-7 col-md-7 col-sm-12" style="margin-top: 120px;">
+                        <a href="index.php">
+                            <img class="" alt="Logo" src="img/ladder-logo.PNG" style="height: 60px; width: 60px; object-fit: contain;" />
+                            <span style="font-family: 'Poppins', sans-serif; font-size: 25px; color: white;">Ladder LLC</span>
+                        </a>
+                    </div>
+                    <div class="col-lg-5 col-md-5 col-sm-12">
+                        <div class="footer-widget mt-30">
+                            <div class="footer-widget-heading" style="margin-top: 100px; margin-bottom: 30px; color: white;">
+                                <h4 style="margin-left: 20px;">Contact Us</h4>
+                            </div>
+                            <div class="single-address" style="color: white;">
+                                <p style="margin-left: 10px;"><i class="fas fa-home" style="margin-right: 10px; color: #f16522;"></i>7288 Hanover Green Drive Mechanicsville, VA 23111</p>
+                            </div>
+                            <div class="single-address" style="color: white;">
+                                <p style="margin-left: 10px;"><i class="far fa-envelope-open" style="margin-right: 10px; color: #f16522;"></i>support@ladderllc.co</p>
+                            </div>
+                            <div class="single-address" style="color: white;">
+                                <p style="margin-left: 10px;"> <i class="fas fa-headphones" style="margin-right: 10px; color: #f16522;"></i>(571)-214-5629</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <script src="assets/v3/application-010e31d394996a79e1a8f5975f1336cc14c8dae69d92bfc42db516fd855e7e2f.js"></script>
+    <script src="assets/v3/smooth-scroll.js"></script>
+    <script src="slick/slick.min.js"></script>
+</body>
+
+</html>
